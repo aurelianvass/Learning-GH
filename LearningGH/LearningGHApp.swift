46c69b8f051db2b2075f010673bddb80aeddf313
Aurelian Vass <aurelian.vass@gmail.com>
2022-09-17 13:18:10 +0300

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aurelian Vass on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
